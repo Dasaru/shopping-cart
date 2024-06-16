b1c36fb7fd487e0e06f5cda6a2ab2c0ff1e64ff1
Dasaru <dberry02jr@yahoo.com>
2024-06-16 14:00:40 -0700

    Add CheckoutRow display and styles

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,37 @@
+import { useContext } from "react";
+import { GlobalStateContext } from "../GlobalStateContext";
+import CheckoutRow from "../components/CheckoutRow";
+
 export default function Checkout() {
+  const { cart, products } = useContext(GlobalStateContext); // TODO: USE THE CART INSTEAD OF PRODUCTS
+
+  const cartSize = products.length; // TODO: Change to cart.length
+
   return (
-    <div className="page-container">
-      <h1>Checkout Page</h1>
+    <div className="page-container checkout-page">
+      {cartSize === 0 ? (
+        <h1>Empty Cart</h1>
+      ) : (
+        <>
+          <h1>Your Cart ({cartSize} items)</h1>
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">Item</th>
+                <th scope="col">Price</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Total</th>
+                <th scope="col" aria-label="Delete"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map(product => (
+                <CheckoutRow key={product.id} product={product} quantity={7} /> 
+              ))}
+            </tbody>
+          </table>
+        </>
+      )}
     </div>
   );
 }
