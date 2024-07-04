194c17c1ca42262ef000c71a99c25ce29cb82b65
Dasaru <dberry02jr@yahoo.com>
2024-07-04 10:00:15 -0700

    Fix product load test

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -5,13 +5,52 @@ import App from "../App";
 import Products from "../pages/Products";
 import { GlobalStateContext } from "../GlobalStateContext";
 
+const mockProducts = [
+  {
+    "id": 1,
+    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    "price": 109.95,
+    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    "category": "men's clothing",
+    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    "rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+  },
+  {
+    "id": 8,
+    "title": "Pierced Owl Rose Gold Plated Stainless Steel Double",
+    "price": 10.99,
+    "description": "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
+    "category": "jewelery",
+    "image": "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
+    "rating": {
+      "rate": 1.9,
+      "count": 100
+    }
+  },
+  {
+    "id": 12,
+    "title": "WD 4TB Gaming Drive Works with Playstation 4 Portable External Hard Drive",
+    "price": 114,
+    "description": "Expand your PS4 gaming experience, Play anywhere Fast and easy, setup Sleek design with high capacity, 3-year manufacturer's limited warranty",
+    "category": "electronics",
+    "image": "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg",
+    "rating": {
+      "rate": 4.8,
+      "count": 400
+    }
+  },
+];
+
 describe("App component", () => {
   it("renders correct product link", () => {
     render(<App />);
     expect(screen.getByRole("link", {name: /Products/i})).toBeInTheDocument();
   });
 
-  it("initial load renders the navbar and product page", () => {
+  it("initially renders the navbar and product page", () => {
     render(<App />);
     expect(screen.queryByTestId("navbar")).toBeInTheDocument();
     expect(screen.queryByTestId("product-page")).toBeInTheDocument();
@@ -59,50 +98,12 @@ describe("App component", () => {
 });
 
 describe("Product page", () => {
-  it("Products are loaded on page", async () => {
-    const mockProducts = [
-      {
-        "id": 1,
-        "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-        "price": 109.95,
-        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-        "category": "men's clothing",
-        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-        "rating": {
-          "rate": 3.9,
-          "count": 120
-        }
-      },
-      {
-        "id": 2,
-        "title": "Mens Casual Premium Slim Fit T-Shirts ",
-        "price": 22.3,
-        "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-        "category": "men's clothing",
-        "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-        "rating": {
-          "rate": 4.1,
-          "count": 259
-        }
-      },
-      {
-        "id": 3,
-        "title": "Mens Cotton Jacket",
-        "price": 55.99,
-        "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
-        "category": "men's clothing",
-        "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-        "rating": {
-          "rate": 4.7,
-          "count": 500
-        }
-      }
-    ];
-    const mockCart = [];
-
+  it("loads the products on the page", async () => {
     const mockContextValues = {
       products: mockProducts,
-      cart: mockCart
+      cart: [],
+      categories: [],
+      activeCategories: []
     };
 
     render(
